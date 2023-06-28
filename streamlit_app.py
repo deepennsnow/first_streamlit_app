a6cf2e1d8bef9329f151ba1a2274586d0c20dbb3
deepennsnow <138068674+deepennsnow@users.noreply.github.com>
2023-06-28 15:48:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,9 @@ import pandas
 # We want pandas to read our CSV file from that S3 bucket so we use a pandas function called read_csv  to pull the data into a dataframe we'll call my_fruit_list.
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+# then tell streamlit to index the fruit column when allowing multiselect below
+my_fruit_list=my_fruit_list.set_index('Fruit')
+
 # After pulling the data into a pandas dataframe called my_fruit_list, we will ask the streamlit library to display it on the page by typing:
 streamlit.dataframe(my_fruit_list)
 
